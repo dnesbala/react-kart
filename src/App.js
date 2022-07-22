a0836c7ca6a0234b31b22f5975f519c4eaf229be
Dinesh Bala <baladnes@gmail.com>
2022-07-22 09:25:33 +0545

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
+import Products from "./components/Products/Products";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
