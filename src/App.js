d9abc68699d10f5a41ed5137dd7d5ee8fe2361ce
Dinesh Bala <baladnes@gmail.com>
2022-07-24 07:43:08 +0545

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import ProductDetail from "./components/ProductDetail/ProductDetail";
 import Products from "./components/Products/Products";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetail />} />
